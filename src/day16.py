a84efe1e088e29a18a3d08b883fb5136c645e460
molleafauss <lmollea@yahoo.it>
2023-06-04 20:58:50 +0100

    Part 2 wip

--- src/day16.py
+++ src/day16.py
@@ -9,7 +9,8 @@ import math
 # problem, where you have to remember your previous steps
 
 RE_VALVE = re.compile(r"Valve (\S+) has flow rate=(\d+); tunnels? leads? to valves? (.*)")
-MAX_MINUTES = 30
+PART1_MINUTES = 30
+PART2_MINUTES = 26
 
 
 @dataclass
@@ -27,10 +28,27 @@ class Path:
     total_flow: int
 
 
+@dataclass
+class Pos:
+    visited: list[str]
+    time: int
+
+
+@dataclass
+class BiPath:
+    human: Pos
+    elephant: Pos
+    remaining_valves: set[str]
+    elapsed: int
+    total_flow: int
+
+
 class Solution(Solver):
     def __init__(self):
         self.valves = {}
         self.cache = {}
+        self.distances = {}
+        self.cache_hits = 0
 
     def parse(self, line: str):
         if not line:
@@ -43,21 +61,26 @@ class Solution(Solver):
         self.valves[valve.name] = valve
 
     def solve(self):
-        print(f"Found {len(self.valves)} valves to open in {MAX_MINUTES} minutes")
+        print(f"Found {len(self.valves)} valves to open in {PART1_MINUTES} minutes")
         start = 'AA'
         valves_with_flow = {v.name for v in self.valves.values() if v.flow > 0}
         print(f"Valves with flow: {len(valves_with_flow)} => {math.factorial(len(valves_with_flow))} possible paths")
         # find distances between all valves with a flow, plus the starting valve (which can have flow also)
-        distances = self.calculate_distances({start} | valves_with_flow)
+        self.calculate_distances({start} | valves_with_flow)
 
-        best_path = self.find_path(Path(["AA"], valves_with_flow, 0, 0), distances)
+        best_path = self.find_path(Path(["AA"], valves_with_flow, 0, 0))
+        print(f"[1] Found max flow is {best_path.total_flow}: {best_path.visited} ({self.cache_hits} cache hits)")
 
-        print(f"[1] Found max flow is {best_path.total_flow}: {best_path.visited}")
+        self.cache_hits = 0
+        self.cache = {}
+        best_path = self.two_paths(BiPath(Pos(['AA'], 0), Pos(['AA'], 0), valves_with_flow, 0, 0))
+        print(f"[2] Found max flow is {best_path.total_flow}: {best_path.human} / {best_path.elephant} ({self.cache_hits} cache hits)")
 
-    def find_path(self, path, distances):
+    def find_path(self, path):
         cave = path.visited[-1]
         cache_key = (cave, path.elapsed, tuple(sorted(path.remaining_valves)))
         if cache_key in self.cache:
+            self.cache_hits += 1
             cached = self.cache[cache_key]
             # add the cached delta to the current status
             return Path(path.visited + cached.visited,
@@ -67,15 +90,15 @@ class Solution(Solver):
 
         best_path = path
         for valve in path.remaining_valves:
-            distance = distances[cave][valve]
+            distance = self.distances[cave][valve]
             elapsed = path.elapsed + distance + 1
-            if elapsed >= MAX_MINUTES:
+            if elapsed >= PART1_MINUTES:
                 # would not be able to do anything in time
                 continue
-            flow = (MAX_MINUTES - elapsed) * self.valves[valve].flow
+            flow = (PART1_MINUTES - elapsed) * self.valves[valve].flow
             sub_best = self.find_path(
                 Path(path.visited + [valve], path.remaining_valves ^ {valve}, elapsed, path.total_flow + flow),
-                distances)
+                )
             if sub_best.total_flow > best_path.total_flow:
                 best_path = sub_best
 
@@ -88,10 +111,72 @@ class Solution(Solver):
         )
         return best_path
 
+    def two_paths(self, path):
+        man_pos = path.human.visited[-1]
+        ele_pos = path.elephant.visited[-1]
+
+        # check cache
+        cache_key = (man_pos, path.human.time, ele_pos, path.elephant.time, tuple(sorted(path.remaining_valves)))
+        if cache_key in self.cache:
+            self.cache_hits += 1
+            cached = self.cache[cache_key]
+            # add the cached delta to the current status
+            return BiPath(
+                Pos(path.human.visited + cached.human.visited, path.human.time + cached.human.time),
+                Pos(path.elephant.visited + cached.elephant.visited, path.elephant.time + cached.elephant.time),
+                cached.remaining_valves,
+                path.elapsed + cached.elapsed,
+                path.total_flow + cached.total_flow
+            )
+
+        best_path = path
+        for valve in path.remaining_valves:
+            # try to move both human and elephant towards the next valve
+            distance = self.distances[man_pos][valve]
+            elapsed = path.human.time + distance + 1
+            if elapsed < PART2_MINUTES:
+                flow = (PART2_MINUTES - elapsed) * self.valves[valve].flow
+                sub_best = self.two_paths(
+                    BiPath(
+                        Pos(path.human.visited + [valve], elapsed),
+                        path.elephant,
+                        path.remaining_valves ^ {valve},
+                        max(elapsed, path.elephant.time),
+                        path.total_flow + flow
+                    )
+                )
+                if sub_best.total_flow > best_path.total_flow:
+                    best_path = sub_best
+
+            distance = self.distances[ele_pos][valve]
+            elapsed = path.elephant.time + distance + 1
+            if elapsed < PART2_MINUTES:
+                flow = (PART2_MINUTES - elapsed) * self.valves[valve].flow
+                sub_best = self.two_paths(
+                    BiPath(
+                        path.human,
+                        Pos(path.elephant.visited + [valve], elapsed),
+                        path.remaining_valves ^ {valve},
+                        max(elapsed, path.elephant.time),
+                        path.total_flow + flow
+                    )
+                )
+                if sub_best.total_flow > best_path.total_flow:
+                    best_path = sub_best
+
+        # I need to cache the "delta" from here to the end
+        self.cache[cache_key] = BiPath(
+            Pos(best_path.human.visited[len(path.human.visited):], best_path.human.time - path.human.time),
+            Pos(best_path.elephant.visited[len(path.elephant.visited):], best_path.elephant.time - path.elephant.time),
+            best_path.remaining_valves,
+            best_path.elapsed - path.elapsed,
+            best_path.total_flow - path.total_flow
+        )
+        return best_path
+
     def calculate_distances(self, valves):
-        distances = {}
         for valve in valves:
-            distances[valve] = {valve: 0}
+            self.distances[valve] = {valve: 0}
             visited = {valve}
             queue = [(0, valve)]
             while queue:
@@ -101,11 +186,11 @@ class Solution(Solver):
                         continue
                     visited.add(next)
                     if next in valves:
-                        distances[valve][next] = distance + 1
+                        self.distances[valve][next] = distance + 1
                     queue.append((distance + 1, next))
 
-            del distances[valve][valve]
-        return distances
+            # needed?
+            del self.distances[valve][valve]
 
     def test_data(self):
         return """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
@@ -122,6 +207,7 @@ Valve JJ has flow rate=21; tunnel leads to valve II
 
     def file_name(self):
         return "../files/day16-caves.txt"
+        # return None
 
 
 
