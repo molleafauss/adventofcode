24e7fc9e492c0ae1d10736421ac0f5e10110efc5
molleafauss <lmollea@yahoo.it>
2023-06-04 18:46:59 +0100

    At last! Part 1 solved!!

--- src/day16.py
+++ src/day16.py
@@ -2,16 +2,15 @@ from dataclasses import dataclass
 
 from advent import Solver
 import re
+import math
 
 # https://adventofcode.com/2022/day/16
-# TODO: INCOMPLETE
-# this doesn't give me the result I was expecting; I was looking to open a valve that would give me the highest flow
-# from where I started, but I believe I am missing the fact that while moving I may meet other valves to open.
-# Need to rethink the algorithm not as the "shortest" path, but the "longest" and from every node.
-
+# this needed a lot of looking at other solutions. Of course this is the usual "dynamic programming" (quotes needed)
+# problem, where you have to remember your previous steps
 
 RE_VALVE = re.compile(r"Valve (\S+) has flow rate=(\d+); tunnels? leads? to valves? (.*)")
 MAX_MINUTES = 30
+
 
 @dataclass
 class Valve:
@@ -21,13 +20,8 @@ class Valve:
 
 
 @dataclass
-class Cave:
-    name: str
-    opened: bool
-
-@dataclass
 class Path:
-    visited: list[Cave]
+    visited: list[str]
     remaining_valves: set[str]
     elapsed: int
     total_flow: int
@@ -36,7 +30,7 @@ class Path:
 class Solution(Solver):
     def __init__(self):
         self.valves = {}
-        self.minutes = 30
+        self.cache = {}
 
     def parse(self, line: str):
         if not line:
@@ -49,63 +43,69 @@ class Solution(Solver):
         self.valves[valve.name] = valve
 
     def solve(self):
-        print(f"Found {len(self.valves)} valves to open in {self.minutes} minutes")
+        print(f"Found {len(self.valves)} valves to open in {MAX_MINUTES} minutes")
         start = 'AA'
         valves_with_flow = {v.name for v in self.valves.values() if v.flow > 0}
-        best_path = Path([Cave("", True)], set(), MAX_MINUTES, 0)
-        paths = [Path([Cave(start, False), Cave(v, False)], valves_with_flow.copy(), 1, 0) for v in self.valves[start].connections]
-        cycles = 0
-        while paths:
-            path = paths.pop(0)
-            cycles += 1
-            if (cycles % 100000) == 0:
-                print(f"{cycles} paths checked, {len(paths)} remaining")
-            if path.elapsed >= MAX_MINUTES:
-                # can't move further
-                best_path = self.check_best(path, best_path, paths)
-                continue
-            # don't move further if there's no way to beat the best (= sum flow as if you opened everything left now)
-            max_flow = sum([self.valves[v].flow * (MAX_MINUTES - path.elapsed) for v in path.remaining_valves])
-            if path.total_flow + max_flow < best_path.total_flow:
-                continue
-            # current cave
-            valve = path.visited[-1]
-            # previous caves visited after latest valve was opened, don't go back there again
-            previous = set()
-            i = len(path.visited) - 1
-            while i >= 0 and not path.visited[i].opened:
-                previous.add(path.visited[i].name)
-                i -= 1
-            # even if there's a valve to open, ignore and pass through, ignoring where you came from (avoid cycles)
-            for v in self.valves[valve.name].connections:
-                if v in previous:
-                    continue
-                paths.append(Path(path.visited + [Cave(v, False)], path.remaining_valves.copy(), path.elapsed + 1, path.total_flow))
-            # no valve to open - visit next path
-            if valve.name not in path.remaining_valves:
-                continue
-            # there's a valve to open
-            path.remaining_valves.remove(valve.name)
-            # stay 1 minute to open valve
-            path.visited.append(Cave(valve.name, True))
-            path.elapsed += 1
-            path.total_flow += self.valves[valve.name].flow * (MAX_MINUTES - path.elapsed)
-            # If all valves are open, stop and check if this is the best path so far
-            if not path.remaining_valves:
-                best_path = self.check_best(path, best_path, paths)
-            else:
-                # else add one new path per every outgoing path from this node, including where you came from
-                for v in self.valves[valve.name].connections:
-                    paths.append(Path(path.visited + [Cave(v, False)], path.remaining_valves.copy(), path.elapsed + 1, path.total_flow))
+        print(f"Valves with flow: {len(valves_with_flow)} => {math.factorial(len(valves_with_flow))} possible paths")
+        # find distances between all valves with a flow, plus the starting valve (which can have flow also)
+        distances = self.calculate_distances({start} | valves_with_flow)
+
+        best_path = self.find_path(Path(["AA"], valves_with_flow, 0, 0), distances)
 
         print(f"[1] Found max flow is {best_path.total_flow}: {best_path.visited}")
 
-    def check_best(self, path, best_path, paths):
-        # check if it is bigger than best path
-        if path.total_flow > best_path.total_flow:
-            print(f"Found a better path: {path.total_flow} > {best_path.total_flow} {path.visited} (remaining: {len(paths)})")
-            return path
+    def find_path(self, path, distances):
+        cave = path.visited[-1]
+        cache_key = (cave, path.elapsed, tuple(sorted(path.remaining_valves)))
+        if cache_key in self.cache:
+            cached = self.cache[cache_key]
+            # add the cached delta to the current status
+            return Path(path.visited + cached.visited,
+                        cached.remaining_valves,
+                        path.elapsed + cached.elapsed,
+                        path.total_flow + cached.total_flow)
+
+        best_path = path
+        for valve in path.remaining_valves:
+            distance = distances[cave][valve]
+            elapsed = path.elapsed + distance + 1
+            if elapsed >= MAX_MINUTES:
+                # would not be able to do anything in time
+                continue
+            flow = (MAX_MINUTES - elapsed) * self.valves[valve].flow
+            sub_best = self.find_path(
+                Path(path.visited + [valve], path.remaining_valves ^ {valve}, elapsed, path.total_flow + flow),
+                distances)
+            if sub_best.total_flow > best_path.total_flow:
+                best_path = sub_best
+
+        # I need to cache the "delta" from here to the end
+        self.cache[cache_key] = Path(
+            best_path.visited[len(path.visited):],
+            best_path.remaining_valves,
+            best_path.elapsed - path.elapsed,
+            best_path.total_flow - path.total_flow
+        )
         return best_path
+
+    def calculate_distances(self, valves):
+        distances = {}
+        for valve in valves:
+            distances[valve] = {valve: 0}
+            visited = {valve}
+            queue = [(0, valve)]
+            while queue:
+                distance, cave = queue.pop(0)
+                for next in self.valves[cave].connections:
+                    if next in visited:
+                        continue
+                    visited.add(next)
+                    if next in valves:
+                        distances[valve][next] = distance + 1
+                    queue.append((distance + 1, next))
+
+            del distances[valve][valve]
+        return distances
 
     def test_data(self):
         return """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
@@ -121,7 +121,7 @@ Valve JJ has flow rate=21; tunnel leads to valve II
 """
 
     def file_name(self):
-        super().file_name()
+        return "../files/day16-caves.txt"
 
 
 
