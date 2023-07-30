def886f9d672adb5a2ad1ddfac6a6e765cf22caa
molleafauss <lmollea@yahoo.it>
2023-07-30 17:57:28 +0100

    Day06 in rust

--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -3,6 +3,7 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 /// A solver for a AOC puzzle
 pub(crate) trait Solver {
@@ -19,6 +20,7 @@ pub(crate) fn solver_for(day: &str) -> Box<dyn Solver> {
         "day03" => Box::new(day03::Solution::new()),
         "day04" => Box::new(day04::Solution::new()),
         "day05" => Box::new(day05::Solution::new()),
+        "day06" => Box::new(day06::Solution::new()),
         _ => panic!("Unsupported puzzle {day}"),
     }
 }
