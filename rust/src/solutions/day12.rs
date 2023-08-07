2bcc36609990e10a7343c584c557e9087ab24851
molleafauss <lmollea@yahoo.it>
2023-08-07 12:34:10 +0100

    Day12 in rust - skeleton

---
+++ rust/src/solutions/day12.rs
@@ -0,0 +1,21 @@
+use crate::Solver;
+
+pub(crate) struct Solution {
+
+}
+
+impl Solution {
+    pub(crate) fn new() -> Solution {
+        Solution {}
+    }
+}
+
+impl Solver for Solution {
+    fn parse(&mut self, line: &str) {
+        todo!()
+    }
+
+    fn solve(&mut self) {
+        todo!()
+    }
+}
