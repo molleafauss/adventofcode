20c4e06f0f3d97a18b5b987c835d51329db1b03d
molleafauss <lmollea@yahoo.it>
2023-08-20 10:17:41 +0100

    Da19 wip

--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -16,6 +16,7 @@ mod day15;
 mod day16;
 mod day17;
 mod day18;
+mod day19;
 
 /// A solver for a AOC puzzle
 pub(crate) trait Solver {
@@ -45,6 +46,7 @@ pub(crate) fn solver_for(day: &str) -> Box<dyn Solver> {
         "day16" => Box::new(day16::Solution::new()),
         "day17" => Box::new(day17::Solution::new()),
         "day18" => Box::new(day18::Solution::new()),
+        "day19" => Box::new(day19::Solution::new()),
         _ => panic!("Unsupported puzzle {day}"),
     }
 }
