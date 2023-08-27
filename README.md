4d001e856b10f71f1e4d9a2a10704e5637d7a6c7
molleafauss <lmollea@yahoo.it>
2023-08-27 19:05:00 +0100

    Added result checking and running all tests

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Advent Of Code 2022
 My solutions for advent of code 2022 (https://adventofcode.com/).
 
-All solutions are posted on the master branch, no commits were cut. Feel free to go through the history.
+All solutions are posted on the master branch, no commits were cut. Feel free to go through the history and see my 
+mistakes.
 
 ## Some general notes
 Although my biggest background is in Java, solutions are in python. Language is great for quick prototyping 
@@ -12,14 +13,14 @@ to be able to solve all them in time. I'm also trying to make code readable and 
 (some exists but are either commented or pointed out in the code and are quite limited to what the language allows).
 I've to say I'm not a fan of such kind of code, true these exercises are just fun stuff, but the ability of writing
 comprehensible code that can be easily interpreted is - imho - a bonus and not a minus (and it's what I tend to point
-out in my code reviews)
-
-There are branches where I tried to port some of the most interesting puzzles to two other languages I am 
-currently interested in learning: Rust and Go. Don't expect these branches to contain all puzzles (they 
-eventually will, but I'll "translate" at my own pace).
-
-After a few days, a certain "pattern" in how to approach solutions started surfacing, thus I abstracted some 
-"plumbing", and each day file only contains the solution. Not sure that idea will support all puzzles till 
-the end.
+out in my code reviews).
 
 Each puzzle may contain some extra notes specific to the solution.
+
+### Refactoring post completion
+Now that I have solved everything, the code is organised in such a way that it will test for expected output. Each 
+input file has the part 1 and part 2 results listed at the top.
+
+### Port to Rust
+I've converted all original solutions to Rust. It's a language I'm interested in, and converting the programs I had 
+already working, made a fun exercise to understand the language and its quirks better. 
--- python/main.py
+++ python/main.py
@@ -7,19 +7,38 @@ import time
 
 
 def solve(filename: pathlib.Path, parser):
+    expected_part_1 = None
+    expected_part_2 = None
     with open(filename) as f:
-        for l in f:
-            parser.parse(l.rstrip())
+        for line in f:
+            if line.startswith("result part 1: "):
+                expected_part_1 = line[15:].strip()
+            elif line.startswith("result part 2: "):
+                expected_part_2 = line[15:].strip()
+            else:
+                parser.parse(line.rstrip())
     t0 = time.time()
-    parser.solve()
+    result = parser.solve()
     t1 = time.time()
     print(f"File {filename}: {t1 - t0:.3f}sec")
+    if not result:
+        return
+    if expected_part_1 and result[0] == expected_part_1:
+        print(f"PART 1 - found expected result: {expected_part_1} = {result[0]}")
+    elif expected_part_1 and result[0] != expected_part_1:
+        print(f"ERROR - part 1 result is incorrect: expected {expected_part_1}, actual {result[0]}")
+    if expected_part_2 and result[1] == expected_part_2:
+        print(f"PART 2 - found expected result: {expected_part_2} = {result[1]}")
+    elif expected_part_2 and result[1] != expected_part_2:
+        print(f"ERROR - part 2 result is incorrect: expected {expected_part_2}, actual {result[1]}")
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        raise Exception("Please specify a day to resolve like 'day03'")
-    day = sys.argv[1]
+def solve_all():
+    for i in range(1, 26):
+        solve_day(f"day{i:02}")
+
+
+def solve_day(day):
     print(f"== Solving {day} ==")
     module = __import__(day)
     # test input
@@ -28,4 +47,14 @@ if __name__ == '__main__':
     # real input
     input_data = pathlib.Path(__file__).parent / f"../inputs/{day}/input.txt"
     solve(input_data, module.Solution())
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        raise Exception("Please specify a day to resolve like 'day03'")
+    day = sys.argv[1]
+    if day == "all":
+        solve_all()
+    else:
+        solve_day(day)
 
