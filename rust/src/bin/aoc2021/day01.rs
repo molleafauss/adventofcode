6989af0bfd77a23472ecc37429671218436b5d8d
molleafauss <lmollea@yahoo.it>
2023-09-02 21:14:37 +0100

    2021 setup

---
+++ rust/src/bin/aoc2021/day01.rs
@@ -0,0 +1,21 @@
+use adventofcode::Solver;
+
+pub struct Solution {
+}
+
+impl Solution {
+    pub(crate) fn new() -> Solution {
+        Solution {
+        }
+    }
+}
+
+impl Solver for Solution {
+    fn parse(&mut self, line: &str) {
+        todo!()
+    }
+
+    fn solve(&mut self) -> Option<(String, String)> {
+        todo!()
+    }
+}
