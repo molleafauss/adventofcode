732c1ec132f1df49c5a7b17344c49d95e218568a
molleafauss <lmollea@yahoo.it>
2023-09-02 21:12:20 +0100

    flattened layout

--- rust/src/bin/aoc2022/main.rs
+++ rust/src/bin/aoc2022/main.rs
@@ -1,34 +1,58 @@
-mod solutions;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
+mod day25;
 
 use adventofcode::Solver;
 
 fn solver_for(day: &str) -> Box<dyn Solver> {
     match day {
-        "day01" => Box::new(solutions::day01::Solution::new()),
-        "day02" => Box::new(solutions::day02::Solution::new()),
-        "day03" => Box::new(solutions::day03::Solution::new()),
-        "day04" => Box::new(solutions::day04::Solution::new()),
-        "day05" => Box::new(solutions::day05::Solution::new()),
-        "day06" => Box::new(solutions::day06::Solution::new()),
-        "day07" => Box::new(solutions::day07::Solution::new()),
-        "day08" => Box::new(solutions::day08::Solution::new()),
-        "day09" => Box::new(solutions::day09::Solution::new()),
-        "day10" => Box::new(solutions::day10::Solution::new()),
-        "day11" => Box::new(solutions::day11::Solution::new()),
-        "day12" => Box::new(solutions::day12::Solution::new()),
-        "day13" => Box::new(solutions::day13::Solution::new()),
-        "day14" => Box::new(solutions::day14::Solution::new()),
-        "day15" => Box::new(solutions::day15::Solution::new()),
-        "day16" => Box::new(solutions::day16::Solution::new()),
-        "day17" => Box::new(solutions::day17::Solution::new()),
-        "day18" => Box::new(solutions::day18::Solution::new()),
-        "day19" => Box::new(solutions::day19::Solution::new()),
-        "day20" => Box::new(solutions::day20::Solution::new()),
-        "day21" => Box::new(solutions::day21::Solution::new()),
-        "day22" => Box::new(solutions::day22::Solution::new()),
-        "day23" => Box::new(solutions::day23::Solution::new()),
-        "day24" => Box::new(solutions::day24::Solution::new()),
-        "day25" => Box::new(solutions::day25::Solution::new()),
+        "day01" => Box::new(day01::Solution::new()),
+        "day02" => Box::new(day02::Solution::new()),
+        "day03" => Box::new(day03::Solution::new()),
+        "day04" => Box::new(day04::Solution::new()),
+        "day05" => Box::new(day05::Solution::new()),
+        "day06" => Box::new(day06::Solution::new()),
+        "day07" => Box::new(day07::Solution::new()),
+        "day08" => Box::new(day08::Solution::new()),
+        "day09" => Box::new(day09::Solution::new()),
+        "day10" => Box::new(day10::Solution::new()),
+        "day11" => Box::new(day11::Solution::new()),
+        "day12" => Box::new(day12::Solution::new()),
+        "day13" => Box::new(day13::Solution::new()),
+        "day14" => Box::new(day14::Solution::new()),
+        "day15" => Box::new(day15::Solution::new()),
+        "day16" => Box::new(day16::Solution::new()),
+        "day17" => Box::new(day17::Solution::new()),
+        "day18" => Box::new(day18::Solution::new()),
+        "day19" => Box::new(day19::Solution::new()),
+        "day20" => Box::new(day20::Solution::new()),
+        "day21" => Box::new(day21::Solution::new()),
+        "day22" => Box::new(day22::Solution::new()),
+        "day23" => Box::new(day23::Solution::new()),
+        "day24" => Box::new(day24::Solution::new()),
+        "day25" => Box::new(day25::Solution::new()),
         _ => panic!("Unsupported puzzle {day}"),
     }
 }
