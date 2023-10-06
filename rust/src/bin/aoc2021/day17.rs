8d91a61cb30add410525f372180b6c3b23d3c388
molleafauss <lmollea@yahoo.it>
2023-10-06 22:41:14 +0100

    Day 17 done

--- rust/src/bin/aoc2021/day17.rs
+++ rust/src/bin/aoc2021/day17.rs
@@ -31,7 +31,7 @@ impl Solution {
         };
         let reachable = self.yrange.0 <= end_y && end_y <= self.yrange.1;
         if reachable {
-            debug!("Reachability found for ({vx}, {vy}) within {t} (open interval: {open_interval}) => {end_y}");
+            debug!("Reachability found for ({vx}, {vy}) within {t} => {end_y}");
             return Some(true);
         }
 
@@ -39,11 +39,11 @@ impl Solution {
         let mut nextt = t + 1;
         let mut nextv = vy - t;
         while open_interval && self.yrange.0 <= end_y {
+            end_y += nextv;
             nextt += 1;
             nextv -= 1;
-            end_y += nextv;
-            if end_y <= self.yrange.1 {
-                debug!("Reachability found for ({vx}, {vy}) within {nextt} [{t} => ...] => {end_y} (at vy {nextv})");
+            if self.yrange.0 <= end_y && end_y <= self.yrange.1 {
+                debug!("Reachability found for ({vx}, {vy}) in [{t} => {nextt}] => {end_y} (vy {} => {nextv})", vy - t);
                 return Some(true)
             }
         }
@@ -53,10 +53,66 @@ impl Solution {
         let min_t = (b + (b.powi(2) - 2.0 * (self.yrange.0 - 1) as f64).sqrt()).floor() as i32;
         if vy < 0 && min_t <= t {
             // stop here - from now on we will never be able to get within range
+            debug!("Stopping aiming down at ({vx}, {vy}) => {min_t}");
             return None
         }
 
         Some(false)
+    }
+
+    fn brute_solve(&self) -> u32 {
+        // very brute force, but we're talking very little numbers...
+
+        // can't go faster than this or it won't even reach within the 1st step
+        let max_vx = self.xrange.1;
+        // won't be going slower than this or it will never reach the border of the trench
+        let min_vx = (((-1.0 + (1.0 + 8.0 * self.xrange.0 as f64).sqrt()) / 2.0).ceil()) as i32;
+        debug!("Min Vx to reach {}: {} => {}", self.xrange.0, min_vx, min_vx * (min_vx + 1) / 2);
+        // can't go higher than this (positive) because when it will land back at y = 0 should at
+        // least be within range in one step
+        let max_vy = self.yrange.0.abs() - 1;
+
+        // now let's keep iterating
+        let mut valid_vectors = 0;
+        for vx in min_vx..max_vx + 1 {
+            let mut vy = max_vy;
+            // will stop if I realise that I end before and beneath the trench
+            let mut aim_lower = true;
+            while aim_lower {
+                let mut dx = vx;
+                let mut dy = vy;
+                let mut t = 0;
+                let mut x = 0;
+                let mut y = 0;
+                loop {
+                    if self.xrange.0 <= x && x <= self.xrange.1
+                        && self.yrange.0 <= y && y <= self.yrange.1 {
+                        valid_vectors += 1;
+                        debug!("({valid_vectors}) ({vx},{vy}) will reach trench at {t} => {x}, {y}");
+                        break;
+                    }
+                    x += dx;
+                    y += dy;
+                    t += 1;
+                    // debug!("... {x}, {y} @ {t}");
+                    if dx > 0 {
+                        dx -= 1;
+                    }
+                    dy -= 1;
+
+                    // too far or too below
+                    if x > self.xrange.1 || y < self.yrange.0 {
+                        // debug!("({vx}, {vy}) @ {t} => unreachable: {x}, {y}");
+                        break;
+                    }
+                }
+                vy -= 1;
+                if vy < self.yrange.0 {
+                    break;
+                }
+            }
+        }
+        valid_vectors
     }
 }
 
@@ -90,19 +146,21 @@ impl Solver for Solution {
         // time (there's plenty), the Vy will gradually reduce, then peak at 0, and then finally start
         // getting down, reaching -(Vy+1) when it crosses 0 going down.
         // This makes the best Vy = 1 less than the bottom of the trench.
-        let max_vy = (self.yrange.0 + 1).abs();
+        let max_vy = self.yrange.0.abs() - 1;
         let max_y = max_vy * (max_vy + 1) / 2;
         info!("[1] Top Vy = {max_vy}, max y reached {max_y}");
 
+        let reachable = self.brute_solve();
+
+        /*
         // now try to find all the valid vx, vy that can hit the target.
         // on the X axis we find the minimum Vx we must have to reach the closes border of the trench
         let min_vx = (((-1.0 + (1.0 + 8.0 * self.xrange.0 as f64).sqrt()) / 2.0).ceil()) as i32;
-        let min_dx = min_vx * (min_vx + 1) / 2;
         debug!("Min Vx to reach {}: {} => {}", self.xrange.0, min_vx, min_dx);
 
         // now loop from max_vy down and see how many hit.
         let mut valid_initials = 0;
-        for vx in (min_vx..self.xrange.1 + 1) {
+        for vx in min_vx..self.xrange.1 + 1 {
             let xtrange = find_xtime_range(vx, self.xrange);
             if xtrange.is_empty() {
                 continue;
@@ -124,9 +182,10 @@ impl Solver for Solution {
                 vy -= 1;
             }
         }
-        info!("[2] Found valid vx/vy pairs: {valid_initials}");
+         */
+        info!("[2] velocity vectors will reach trench: {}", reachable);
 
-        Some((max_y.to_string(), valid_initials.to_string()))
+        Some((max_y.to_string(), reachable.to_string()))
     }
 }
 
