57f43e04a354bc819efb5c8a7e1fa7b6ec31f272
molleafauss <lmollea@yahoo.it>
2024-12-01 23:20:20 +0000

    Day 1 part 2

--- go/cmd/aoc2024/day01.go
+++ go/cmd/aoc2024/day01.go
@@ -34,12 +34,14 @@ func (solver *day01) Parse(line string) {
 }
 
 func (solver *day01) Solve() (*string, *string) {
+	aoc.Info("location sizes %d/%d", len(solver.left), len(solver.right))
 	if len(solver.left) != len(solver.right) {
-		aoc.Error("location sizes are uneven?? %d/%d", len(solver.left), len(solver.right))
+		aoc.Error("location sizes are uneven??")
 		return nil, nil
 	}
 	slices.Sort(solver.left)
 	slices.Sort(solver.right)
+	// part 1
 	total := 0
 	for i := range len(solver.left) {
 		delta := solver.left[i] - solver.right[i]
@@ -49,5 +51,34 @@ func (solver *day01) Solve() (*string, *string) {
 		total += delta
 	}
 	part1 := strconv.Itoa(total)
-	return &part1, nil
+
+	// part 2 - pop from left until empty and count on right how many times it happens
+	// lists are sorted so no need keep going through the list
+	similarity := 0
+	l := 0
+	r := 0
+	count := 0
+	for l < len(solver.left) {
+		if l > 0 && solver.left[l] == solver.left[l-1] {
+			similarity += solver.left[l] * count
+			l++
+			continue
+		}
+		// is the current left item > than the current right? advance right
+		for solver.right[r] < solver.left[l] {
+			r++
+		}
+		// reset count
+		count = 0
+		// count how many are equal
+		for solver.right[r] == solver.left[l] {
+			r++
+			count++
+		}
+		// add to similarity
+		similarity += solver.left[l] * count
+		l++
+	}
+	part2 := strconv.Itoa(similarity)
+	return &part1, &part2
 }
