759f586203074ac99b461f6ef8ccad97c6d10b89
alessandro.malgaroli <alessandro.malgaroli@scopely.com>
2024-12-22 18:45:04 +0100

    Day 13 part 2

--- go/cmd/aoc2024/day13.go
+++ go/cmd/aoc2024/day13.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"aoc/aoc"
+	"math/big"
 	"regexp"
 	"strconv"
 )
@@ -24,6 +25,8 @@ func Day13() aoc.Solver {
 
 var RE_BUTTON = regexp.MustCompile("Button (.): X\\+(\\d+), Y\\+(\\d+)")
 var RE_PRIZE = regexp.MustCompile("Prize: X=(\\d+), Y=(\\d+)")
+
+const DELTA_PART2 = 10000000000000
 
 func (solver *day13) Parse(line string) {
 	if len(line) == 0 {
@@ -60,82 +63,51 @@ func (solver *day13) Parse(line string) {
 
 func (solver *day13) Solve() (*string, *string) {
 	coins1 := 0
+	coins2 := 0
 	for _, machine := range solver.machines {
-		coins := findCoins(machine)
-		if coins == -1 {
-			continue
+		coins := findCoins(machine, 0)
+		if coins != -1 {
+			coins1 += coins
 		}
-		coins1 += coins
+		coins = findCoins(machine, DELTA_PART2)
+		if coins != -1 {
+			coins2 += coins
+		}
 	}
 	part1 := strconv.Itoa(coins1)
-	return &part1, nil
+	part2 := strconv.Itoa(coins2)
+	return &part1, &part2
 }
 
-func findCoins(machine claw) int {
-	// impossible to reach
-	if machine.buttonA.Row*100+machine.buttonB.Row*100 < machine.prize.Row ||
-		machine.buttonA.Col*100+machine.buttonB.Col*100 < machine.prize.Col {
+func findCoins(machine claw, delta int) int {
+	// there's probably only one solution for this system
+	// { Ax * m + Bx * m = Px
+	// { Ay * m + By * m = Py
+
+	Ax := int64(machine.buttonA.Col)
+	Ay := int64(machine.buttonA.Row)
+	Bx := int64(machine.buttonB.Col)
+	By := int64(machine.buttonB.Row)
+	Px := int64(machine.prize.Col + delta)
+	Py := int64(machine.prize.Row + delta)
+
+	mRes := big.NewRat(Px, Ax)
+	mRes.Sub(mRes, big.NewRat(Py, Ay))
+	mDen := big.NewRat(Bx, Ax)
+	mDen.Sub(mDen, big.NewRat(By, Ay))
+	mRes.Quo(mRes, mDen)
+	if mRes.Sign() < 0 || !mRes.IsInt() {
+		return -1
+	}
+	m := mRes.Num().Int64()
+
+	// try to find N
+	n := big.NewRat(Px-(Bx*m), Ax)
+	if n.Sign() < 0 || !n.IsInt() {
 		return -1
 	}
 
-	// see if one button only can surpass
-	maxAx := machine.prize.Col / machine.buttonA.Col
-	maxAy := machine.prize.Row / machine.buttonA.Row
-	pressA := min(maxAx, maxAy, 100)
-	maxBx := machine.prize.Col / machine.buttonB.Col
-	maxBy := machine.prize.Row / machine.buttonB.Row
-	pressB := min(maxBx, maxBy, 100)
-	aoc.Info("Would either press A %d or B %d", pressA, pressB)
+	coins := n.Num().Int64()*3 + m
 
-	count := min(pressA, pressB)
-	claw1 := machine.buttonA
-	cost1 := 3
-	claw2 := machine.buttonB
-	cost2 := 1
-	if pressB < pressA {
-		// invert
-		claw2, claw1 = claw1, claw2
-		cost2, cost1 = cost1, cost2
-	}
-
-	// max is 100*3+100
-	minCost := 500
-	for ; count >= 0; count-- {
-		// every time I find an exact match I record presses and cost
-		press2 := willReach(machine.prize.Row, claw1.Row*count, claw2.Row)
-		if press2 == -1 {
-			continue
-		}
-		if press2 > 100 {
-			// stop here - I am too far
-			break
-		}
-		if claw1.Col*count+claw2.Col*press2 != machine.prize.Col {
-			continue
-		}
-		cost := cost1*count + cost2*press2
-		if cost < minCost {
-			minCost = cost
-			aoc.Info("Found better solution with %dx%d - %dx%d = %d", count, cost1, press2, cost2, cost)
-		}
-	}
-
-	// no solution found
-	if minCost == 500 {
-		return -1
-	}
-
-	return minCost
-}
-
-func willReach(end int, start int, delta int) int {
-	if start > end {
-		return -1
-	}
-	remainder := end - start
-	count := remainder / delta
-	if delta*count == remainder {
-		return count
-	}
-	return -1
+	return int(coins)
 }
