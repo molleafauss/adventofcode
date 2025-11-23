dc607cdc18a44ac59bfca45a17a5ba1cb99e6357
alessandro.malgaroli <alessandro.malgaroli@scopely.com>
2025-11-23 19:46:37 +0100

    scaffolding

---
+++ csharp/year2025/Day01.cs
@@ -0,0 +1,15 @@
+namespace adventofcode.year2025
+{
+    public class Day01 : ISolver
+    {
+        public void Parse(string input)
+        {
+        }
+
+        public (string? part1, string? part2) Solve()
+        {
+            Console.WriteLine("--- 2025/01 ---");
+            return (null, null);
+        }
+    }
+}
