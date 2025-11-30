c1a9c75d28bf6b43e6c3fa9b726dccc69e270891
molleafauss <lmollea@yahoo.it>
2025-11-30 18:58:18 +0000

    better cli for java, support all parameters

--- java/src/main/java/aoc/Main.java
+++ java/src/main/java/aoc/Main.java
@@ -1,57 +1,125 @@
 package aoc;
 
 import aoc.api.Solver;
-import aoc.api.SolverFactory;
 import aoc.util.Log;
 import aoc.util.LogLevel;
-import aoc.year2022.Solvers;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.IDefaultValueProvider;
+import picocli.CommandLine.Model.ArgSpec;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Spec;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.time.LocalDate;
 import java.util.Objects;
+import java.util.concurrent.Callable;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Log.setLogLevel(LogLevel.INFO);
-        if (args.length != 1) {
-            System.out.println("Please specify a day to resolve like 'day03'");
-            System.exit(1);
-        }
-        var factory = new Solvers();
-        if (args[0].equals("all")) {
-            solveAll(factory);
-        } else {
-            solveDay(args[0], factory);
+@Command(name = "aoc", mixinStandardHelpOptions = true, defaultValueProvider =
+        Main.DefaultValues.class,
+        description = "Solves Advent of Code puzzles")
+public class Main implements Callable<Integer> {
+
+    static class DefaultValues implements IDefaultValueProvider {
+        @Override
+        public String defaultValue(ArgSpec argSpec) {
+            if (argSpec.paramLabel().equals("<year>")) {
+                var now = LocalDate.now();
+                return String.valueOf(now.getMonthValue() >= 12 ? now.getYear() : now.getYear() - 1);
+            }
+            return "";
         }
     }
 
-    private static void solveDay(String day, SolverFactory factory) throws IOException {
+    @Spec
+    CommandSpec spec;
+
+    @Option(names = "--inputs", description = "Directory to read input files from, default current directory.")
+    private File inputsDir = new File(".");
+
+    @Option(names = "--year", description = "Year of the Advent of Code event - default last available year.")
+    private String year;
+
+    private String dayArg;
+
+    @Option(names = "--debug", description = "Enable debug log.")
+    private boolean debug = false;
+
+    public static void main(String[] args) {
+        System.exit(new CommandLine(new Main()).execute(args));
+    }
+
+    @Parameters(description = "Day to solve (specified as 'dayNN' or 'all' to solve all days in sequence).")
+    public void setDay(String dayArg) {
+        if (!Objects.equals(dayArg, "all") && !dayArg.startsWith("day")) {
+            throw new CommandLine.ParameterException(spec.commandLine(),
+                    "Invalid day parameter: %s. Must be 'dayNN' or 'all'.".formatted(dayArg));
+        }
+        this.dayArg = dayArg;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        validateInputDir();
+
+        Log.setLogLevel(debug ? LogLevel.DEBUG : LogLevel.INFO);
+
+        if (dayArg.equals("all")) {
+            solveAll();
+        } else {
+            solveDay(dayArg);
+        }
+        return 0;
+    }
+
+    private void validateInputDir() {
+        if (!inputsDir.exists() || !inputsDir.isDirectory()) {
+            throw new CommandLine.ParameterException(spec.commandLine(),
+                    "inputs directory %s does not exist or is not a directory".formatted(inputsDir));
+        }
+        // resolve full path
+        inputsDir = inputsDir.getAbsoluteFile();
+    }
+
+    private void solveDay(String day) throws IOException {
         String data = day.substring(0, 5);
-        Log.info("== Solving %s ==", data );
+        Log.info("== Solving %s / %s ==", year, data);
 
         // assume 'input' is a directory in the current directory
-        var testFile = new File("inputs/%s/%s/test.txt".formatted(factory.getYear(), data ));
+        var testFile = new File("%s/%s/%s/test.txt".formatted(inputsDir, year, data));
         if (!testFile.exists()) {
             Log.error("ERROR: test file %s does not exist", testFile);
             System.exit(-1);
         }
-        solve(testFile, factory.createSolver(day));
+        solve(testFile, createSolver(day));
 
-        var inputFile = new File("inputs/%s/%s/input.txt".formatted(factory.getYear(), data ));
+        var inputFile = new File("%s/%s/%s/input.txt".formatted(inputsDir, year, data));
         if (!inputFile.exists()) {
             Log.error("ERROR: input file %s does not exist".formatted(inputFile));
             System.exit(-1);
         }
-        solve(inputFile, factory.createSolver(day));
+        solve(inputFile, createSolver(day));
 
     }
 
-    private static void solveAll(SolverFactory factory) throws IOException {
+    private Solver createSolver(String day) {
+        String className = "aoc.year%s.%s%s".formatted(year, Character.toUpperCase(day.charAt(0)), day.substring(1));
+        try {
+            return (Solver) Class.forName(className).getDeclaredConstructor().newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Cannot create solver for %s".formatted(className), e);
+        }
+    }
+
+    private void solveAll() throws IOException {
         for (int d = 1; d < 26; d++) {
             var day = String.format("day%02d", d);
-            solveDay(day, factory);
+            solveDay(day);
         }
     }
 
