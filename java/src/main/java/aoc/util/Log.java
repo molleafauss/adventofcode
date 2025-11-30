99c24104c792f0b065fa35808bb1baa62ec0602e
molleafauss <lmollea@yahoo.it>
2025-11-30 19:09:40 +0000

    collapsed Log in single file

--- java/src/main/java/aoc/util/Log.java
+++ java/src/main/java/aoc/util/Log.java
@@ -1,31 +1,48 @@
 package aoc.util;
 
 public class Log {
-    private static LogLevel configuredLogLevel = LogLevel.INFO;
+    public enum Level {
+        DEBUG(10),
+        INFO(20),
+        WARN(30),
+        ERROR(40);
 
-    public static void setLogLevel(LogLevel logLevel) {
-        configuredLogLevel = logLevel;
+        private final int value;
+
+        Level(int value) {
+            this.value = value;
+        }
+
+        public int value() {
+            return value;
+        }
+    }
+
+    private static Level configuredLevel = Level.INFO;
+
+    public static void setLogLevel(Level level) {
+        configuredLevel = level;
     }
 
     public static void debug(String msg, Object... args) {
-        doLog(LogLevel.DEBUG, msg, args);
+        doLog(Level.DEBUG, msg, args);
     }
 
     public static void info(String msg, Object... args) {
-        doLog(LogLevel.INFO, msg, args);
+        doLog(Level.INFO, msg, args);
     }
 
     public static void warn(String msg, Object... args) {
-        doLog(LogLevel.WARN, msg, args);
+        doLog(Level.WARN, msg, args);
     }
 
     public static void error(String msg, Object... args) {
-        doLog(LogLevel.ERROR, msg, args);
+        doLog(Level.ERROR, msg, args);
     }
 
-    private static void doLog(LogLevel logLevel, String msg, Object[] args) {
-        if (configuredLogLevel.value() <= logLevel.value()) {
-            System.out.println(logLevel.name() + " | " + String.format(msg, args));
+    private static void doLog(Level level, String msg, Object[] args) {
+        if (configuredLevel.value() <= level.value()) {
+            System.out.println(level.name() + " | " + String.format(msg, args));
         }
     }
 }
