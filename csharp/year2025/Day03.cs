9c5168bab20b6380f7891397437221224cfeba4d
molleafauss <lmollea@yahoo.it>
2025-12-13 19:20:28 +0000

    day 3 part 2

--- csharp/year2025/Day03.cs
+++ csharp/year2025/Day03.cs
@@ -9,37 +9,82 @@ namespace adventofcode.year2025
         
         public void Parse(string input)
         {
-            // scan through the string and find the highest digit first, then find the highest
-            // happening after the first position
-            int p = 0;
-            int d1 = 0;
-            int d2 = 0;
-            for (var i = 0; i < input.Length - 1; i++)
+            var jolt = FindLargestJolt(input, 2);
+            Log.Info($"J2 => {jolt}");
+            _part1 += jolt; 
+            jolt = FindLargestJolt(input, 12);
+            Log.Info($"J12 => {jolt}");
+            _part2 += jolt;
+        }
+
+        private long FindLargestJolt(string input, int size)
+        {
+            // start from the last "size" digits and then proceed "backward": take the highest digit
+            // and find the highest (if present) in the positions from 0 to Lenght - size, then do
+            // the same with the others, until a max is found.
+            var jolt = new Jolt(input, size);
+
+            // loop on each digit starting from the "highest"
+            for (var d = 0; d < size; d++)
             {
-                var d = input[i] - '0';
-                if (d > d1)
+                // loop on the input "backward" from the digit position
+                for (var i = jolt.Digits[d].Position - 1; i >= jolt.GetMinPos(d); i--)
                 {
-                    p = i;
-                    d1 = d;
+                    var v = input[i] - '0';
+                    if (v >= jolt.Digits[d].Value)
+                    {
+                        // found a higher digit, update the jolt
+                        jolt.ReplaceDigit(d, v, i);
+                    }
                 }
             }
-            // second battery position
-            for (var i = p + 1; i < input.Length; i++)
-            {
-                var d = input[i] - '0';
-                if (d > d2)
-                {
-                    d2 = d;
-                }
-            }
-            var jolt = d1 * 10 + d2;
-            Log.Info($"{input} => {jolt}");
-            _part1 += d1 * 10 + d2;
+            return jolt.Value;
         }
 
         public (string? part1, string? part2) Solve()
         {
             return (_part1.ToString(), _part2.ToString());
+        }
+    }
+
+    record Digit(int Value, int Position);
+
+    class Jolt
+    {
+        internal long Value { get; private set; }
+        internal readonly Digit[] Digits;
+
+        internal Jolt(string input, int size)
+        {
+            Digits = new Digit[size];
+            for (var i = 0; i < size; i++)
+            {
+                var v = input[^(size - i)] - '0';
+                Digits[i] = new Digit(v, input.Length - size + i);
+                Value = Value * 10 + v;
+            }
+        }
+        
+        public override string ToString()
+        {
+            var rv = $"{Value} [";
+            foreach (var d in Digits)
+            {
+                rv += $" {d.Value}({d.Position}),";
+            }
+            return rv + "]";
+        }
+
+        public void ReplaceDigit(int digit, int val, int pos)
+        {
+            Value -= Digits[digit].Value * (long)Math.Pow(10, Digits.Length - digit - 1);
+            Value += val * (long)Math.Pow(10, Digits.Length - digit - 1);
+            Digits[digit] = new Digit(val, pos);
+        }
+
+        public int GetMinPos(int digit)
+        {
+            return digit == 0 ? 0 : Digits[digit - 1].Position + 1;
         }
     }
 }
