2f73d2b1aa060b8f6e1399be5c2a83069d0e0f76
molleafauss <lmollea@yahoo.it>
2025-12-29 16:25:49 +0000

    day 16 optimised

--- csharp/year2022/Day16.cs
+++ csharp/year2022/Day16.cs
@@ -11,9 +11,8 @@ public class Day16 : ISolver
     private const int Part2Minutes = 26;
     private const string Start = "AA";
 
-    private readonly List<string> _valvesWithFlow = [];
-    private readonly List<Valve> _valves = [];
-    private readonly Dictionary<string, Dictionary<string, int>> _distances = new();
+    private readonly Dictionary<string, Valve> _valves = new();
+    private readonly Dictionary<string, List<string>> _connections = new();
 
     // Parse a single line of the input
     public void Parse(string line)
@@ -23,12 +22,8 @@ public class Day16 : ISolver
         if (m.Success)
         {
             var valve = new Valve(m);
-            if (valve.Flow > 0)
-            {
-                _valvesWithFlow.Add(valve.Name);
-                valve.Mask = 1 << _valvesWithFlow.Count;
-            }
-            _valves.Add(valve);
+            _connections[valve.Name] = m.Groups[3].Value.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            _valves[valve.Name] = valve;
         }
     }
 
@@ -36,80 +31,73 @@ public class Day16 : ISolver
     public (string? part1, string? part2) Solve()
     {
         Log.Debug($"Found {_valves.Count} valves to open in {Part1Minutes} minutes");
-        CalculateDistances();
+        
+        var valvesWithFlow = PrecacheValues();
 
         // part 1 - timed
         var sw = Stopwatch.StartNew();
-        var onePathSolver = new OnePathSolver(this);
-        var bestPath1 = onePathSolver.FindPath(new OnePath(Start));
+        var onePathSolver = new OnePathSolver(valvesWithFlow);
+        var bestPath1 = onePathSolver.FindPath(new OnePath(_valves[Start]));
         sw.Stop();
-        Log.Info(string.Format("[1] Found max flow is {0}: {1} ({2} cache hits, {3} calls, {4} cache size) [{5:F3}sec]",
-            bestPath1.TotalFlow,
-            string.Join("->", bestPath1.Visited),
-            onePathSolver.CacheHits,
-            onePathSolver.Calls,
-            onePathSolver.Cache.Count,
-            sw.Elapsed.TotalSeconds));
+        Log.Info(
+            $"[1] Found max flow is {bestPath1.TotalFlow}: {string.Join("->", bestPath1.Visited)} ({onePathSolver.CacheHits} cache hits, {onePathSolver.Calls} calls, {onePathSolver.Cache.Count} cache size) [{sw.Elapsed.TotalSeconds:F3}sec]");
 
         // part 2 - timed
         sw.Restart();
-        var twoPathSolver = new TwoPathSolver(this);
-        var bestPath2 = twoPathSolver.FindPath(new TwoPath(Start));
+        var twoPathSolver = new TwoPathSolver(valvesWithFlow);
+        var bestPath2 = twoPathSolver.FindPath(new TwoPath(valvesWithFlow.Count));
         sw.Stop();
-        Log.Info(string.Format("[2] Found max flow is {0}: {1} / {2} ({3} cache hits, {4} calls, {5} cache size) [{6:F3}sec]",
-            bestPath2.TotalFlow,
-            string.Join("->", bestPath2.HumanPath),
-            string.Join("->", bestPath2.ElePath),
-            twoPathSolver.CacheHits,
-            twoPathSolver.Calls,
-            twoPathSolver.Cache.Count,
-            sw.Elapsed.TotalSeconds));
-
+        Log.Info(
+            $"[2] Found max flow is {bestPath2.TotalFlow}: {string.Join("->", bestPath2.HumanPath)} / {string.Join("->", bestPath2.ElePath)} ({twoPathSolver.CacheHits} cache hits, {twoPathSolver.Calls} calls, {twoPathSolver.Cache.Count} cache size) [{sw.Elapsed.TotalSeconds:F3}sec]");
         return (bestPath1.TotalFlow.ToString(), bestPath2.TotalFlow.ToString());
     }
 
-    private void CalculateDistances()
+    private List<Valve> PrecacheValues()
     {
-        // add (temporarily) the start into the valves that need to be evaluated
-        _valvesWithFlow.Insert(0, Start);
-        foreach (var name in _valvesWithFlow)
+        List<Valve> valvesWithFlow = [];
+        foreach (var valve in _valves.Values.Where(valve => valve.Flow > 0))
         {
-            var currentDistances = new Dictionary<string, int> { { name, 0 } };
-            var visited = new HashSet<string> { name };
-            var queue = new Queue<(string, int)>();
-            queue.Enqueue((name, 0));
+            valve.Mask = 1 << valvesWithFlow.Count;
+            valvesWithFlow.Add(valve);
+            // id = 0 will be the START valve
+            valve.Id = (byte) valvesWithFlow.Count;
+        }
+        valvesWithFlow.Insert(0, _valves[Start]);
+
+        foreach (var curr in valvesWithFlow)
+        {
+            curr.Tunnels = new byte[valvesWithFlow.Count];
+            var visited = new HashSet<string> { curr.Name };
+            var queue = new Queue<(Valve, int)>();
+            queue.Enqueue((curr, 0));
             while (queue.Count > 0)
             {
-                var (cave, distance) = queue.Dequeue();
-                var valve = GetValve(cave);
-                foreach (var next in valve.Connections)
+                var (valve, distance) = queue.Dequeue();
+                foreach (var next in _connections[valve.Name])
                 {
                     if (!visited.Add(next)) continue;
-                    if (_valvesWithFlow.Any(v => v == next))
+                    if (valvesWithFlow.Contains(_valves[next]))
                     {
-                        currentDistances[next] = distance + 1;
+                        curr.Tunnels[_valves[next].Id] = (byte)(distance + 1);
                     }
-                    queue.Enqueue((next, distance + 1));
+                    queue.Enqueue((_valves[next], distance + 1));
                 }
             }
-            currentDistances.Remove(name);
-            _distances[name] = currentDistances;
         }
-        // remove the temporary start
-        _valvesWithFlow.RemoveAt(0);
+        
+        return valvesWithFlow;
     }
-
-    private Valve GetValve(string cave) => _valves.First(v => v.Name == cave);
 
     private sealed class Valve(Match m)
     {
-        public List<string> Connections { get; } = m.Groups[3].Value.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
-        public string Name { get; } = m.Groups[1].Value;
-        public int Flow { get; } = int.Parse(m.Groups[2].Value);
-        public int Mask { get; set; }
+        public byte Id;
+        public byte[] Tunnels = [];
+        public readonly string Name = m.Groups[1].Value;
+        public readonly int Flow = int.Parse(m.Groups[2].Value);
+        public int Mask;
     }
 
-    private sealed class OnePathSolver(Day16 parent)
+    private sealed class OnePathSolver(List<Valve> valvesWithFlow)
     {
         public int Calls;
         public readonly Dictionary<OnePathKey, OnePath> Cache = new();
@@ -120,7 +108,6 @@ public class Day16 : ISolver
             Calls += 1;
             if (Calls % 1000000 == 0) Log.Info($"{Calls} calls, {CacheHits} cache hits...");
 
-            var cave = path.Visited[^1];
             var cacheKey = path.CacheKey();
             if (Cache.TryGetValue(cacheKey, out var cached))
             {
@@ -128,12 +115,13 @@ public class Day16 : ISolver
                 return path.Merge(cached);
             }
 
+            var currValve = path.Visited[^1];
             var bestPath = path;
-            foreach (var name in parent._valvesWithFlow)
+            foreach (var valve in valvesWithFlow)
             {
-                var valve = parent.GetValve(name);
+                if (valve.Flow == 0) continue;
                 if ((path.OpenValves & valve.Mask) != 0) continue;
-                var distance = parent._distances[cave][name];
+                var distance = currValve.Tunnels[valve.Id];
                 var next = path.Next(valve, distance);
                 if (next.Elapsed >= Part1Minutes) continue;
                 var subBest = FindPath(next);
@@ -149,12 +137,12 @@ public class Day16 : ISolver
 
     private sealed class OnePath
     {
-        public List<string> Visited { get; }
+        public readonly List<Valve> Visited;
         public int OpenValves { get; }
         public int Elapsed { get; }
         public int TotalFlow { get; }
 
-        public OnePath(string start)
+        public OnePath(Valve start)
         {
             Visited = [start];
             OpenValves = 0;
@@ -162,7 +150,7 @@ public class Day16 : ISolver
             TotalFlow = 0;
         }
 
-        private OnePath(List<string> visited, int openValves, int elapsed, int totalFlow)
+        private OnePath(List<Valve> visited, int openValves, int elapsed, int totalFlow)
         {
             Visited = visited;
             OpenValves = openValves;
@@ -170,18 +158,18 @@ public class Day16 : ISolver
             TotalFlow = totalFlow;
         }
 
-        public OnePathKey CacheKey() => new(Visited[^1], Elapsed, OpenValves);
+        public OnePathKey CacheKey() => new(Visited[^1].Name, Elapsed, OpenValves);
 
         public OnePath Merge(OnePath other)
         {
-            var visited = new List<string>(Visited);
+            var visited = Visited[..];
             visited.AddRange(other.Visited);
             return new OnePath(visited, OpenValves, Elapsed + other.Elapsed, TotalFlow + other.TotalFlow);
         }
 
         public OnePath Next(Valve valve, int distance)
         {
-            var visited = new List<string>(Visited) { valve.Name };
+            var visited = new List<Valve>(Visited) { valve };
             var elapsed = Elapsed + distance + 1;
             var flow = (Part1Minutes - elapsed) * valve.Flow;
             return new OnePath(visited, OpenValves | valve.Mask, elapsed, TotalFlow + flow);
@@ -189,13 +177,13 @@ public class Day16 : ISolver
 
         public OnePath Diff(OnePath start)
         {
-            var visited = new List<string>(Visited.GetRange(start.Visited.Count, Visited.Count - start.Visited.Count));
+            var visited = Visited[start.Visited.Count..];
             return new OnePath(visited, OpenValves, Elapsed - start.Elapsed, TotalFlow - start.TotalFlow);
         }
     }
 
 
-    private sealed class TwoPathSolver(Day16 parent)
+    private sealed class TwoPathSolver(List<Valve> valvesWithFlow)
     {
         public int Calls;
         public readonly Dictionary<TwoPathKey, TwoPath> Cache = new();
@@ -206,24 +194,26 @@ public class Day16 : ISolver
             Calls += 1;
             if (Calls % 1000000 == 0) Log.Info($"{Calls} calls, {CacheHits} cache hits...");
 
-            var manPos = path.HumanPath[^1];
-            var elePos = path.ElePath[^1];
             var cacheKey = path.CacheKey();
-
             if (Cache.TryGetValue(cacheKey, out var cached))
             {
                 CacheHits += 1;
                 return path.Merge(cached);
             }
 
+            var manPos = path.HumanPath[path.HumanPathPos];
+            var manValve = valvesWithFlow[manPos];
+            var elePos = path.ElePath[path.ElePathPos];
+            var eleValve = valvesWithFlow[elePos];
+            
             var bestPath = path;
-            foreach (var name in parent._valvesWithFlow)
+            foreach (var valve in valvesWithFlow)
             {
-                var valve = parent.GetValve(name);
+                if (valve.Flow == 0) continue;
                 if ((path.OpenValves & valve.Mask) != 0) continue;
 
                 // move human
-                var distance = parent._distances[manPos][name];
+                var distance = manValve.Tunnels[valve.Id];
                 var next = path.NextHuman(valve, distance);
                 if (next.Elapsed < Part2Minutes)
                 {
@@ -232,7 +222,7 @@ public class Day16 : ISolver
                 }
 
                 // move elephant
-                distance = parent._distances[elePos][name];
+                distance = eleValve.Tunnels[valve.Id];
                 next = path.NextElephant(valve, distance);
                 if (next.Elapsed < Part2Minutes)
                 {
@@ -246,74 +236,126 @@ public class Day16 : ISolver
         }
     }
 
-    private record TwoPathKey(string HumanPos, int HumanElapsed, string ElePos, int EleElapsed, int Valves);
+    private record TwoPathKey(byte HumanPos, byte HumanElapsed, byte ElePos, byte EleElapsed, int Valves);
 
     private sealed class TwoPath
     {
-        public List<string> HumanPath { get; }
-        private int HumanElapsed { get; }
-        public List<string> ElePath { get; }
-        private int EleElapsed { get; }
+        public byte[] HumanPath { get; }
+        public byte HumanPathPos { get; }
+        private byte HumanElapsed { get; }
+        public byte[] ElePath { get; }
+        public byte ElePathPos { get; }
+        private byte EleElapsed { get; }
         public int OpenValves { get; }
-        public int Elapsed { get; }
+        public byte Elapsed { get; }
         public int TotalFlow { get; }
 
-        private TwoPath(List<string> humanPath, int humanElapsed, List<string> elePath, int eleElapsed, int openValves, int elapsed, int totalFlow)
+        public TwoPath(int size) {
+            HumanPath = new byte[size + 1];
+            HumanPath[0] = 0;
+            ElePath = new byte[size + 1];
+            ElePath[0] = 0;
+        }
+
+        private TwoPath(byte[] humanPath, byte humanPathPos, byte humanElapsed, byte[] elePath, byte elePathPos, byte eleElapsed, int openValves, byte elapsed, int totalFlow)
         {
             HumanPath = humanPath;
+            HumanPathPos = humanPathPos;
             HumanElapsed = humanElapsed;
             ElePath = elePath;
+            ElePathPos = elePathPos;
             EleElapsed = eleElapsed;
             OpenValves = openValves;
             Elapsed = elapsed;
             TotalFlow = totalFlow;
         }
 
-        public TwoPath(string start)
-        {
-            HumanPath = [start];
-            ElePath = [start];
-            HumanElapsed = 0;
-            EleElapsed = 0;
-            OpenValves = 0;
-            Elapsed = 0;
-            TotalFlow = 0;
-        }
-
-        public TwoPathKey CacheKey() => new(HumanPath[^1], HumanElapsed, ElePath[^1], EleElapsed, OpenValves);
+        public TwoPathKey CacheKey() => new(HumanPath[HumanPathPos], HumanElapsed, ElePath[ElePathPos], EleElapsed, OpenValves);
 
         public TwoPath Merge(TwoPath other)
         {
-            var humanPath = new List<string>(HumanPath);
-            humanPath.AddRange(other.HumanPath);
-            var elePath = new List<string>(ElePath);
-            elePath.AddRange(other.ElePath);
-            return new TwoPath(humanPath, HumanElapsed + other.HumanElapsed, elePath, EleElapsed + other.EleElapsed, OpenValves, Elapsed + other.Elapsed, TotalFlow + other.TotalFlow);
+            var humanPath = new byte[HumanPath.Length];
+            if (HumanPathPos > 0) HumanPath[..HumanPathPos].CopyTo(humanPath, 0);
+            if (other.HumanPathPos > 0) other.HumanPath[..other.HumanPathPos].CopyTo(humanPath, HumanPathPos);
+            var elePath = new byte[ElePath.Length];
+            if (ElePathPos > 0) ElePath[..ElePathPos].CopyTo(elePath, 0);
+            if (other.ElePathPos > 0)
+            {
+                other.ElePath[..other.ElePathPos].CopyTo(elePath, ElePathPos);
+            }
+            return new TwoPath(
+                humanPath, 
+                (byte)(HumanPathPos + other.HumanPathPos + 1), 
+                (byte)(HumanElapsed + other.HumanElapsed), 
+                elePath,
+                (byte)(ElePathPos + other.ElePathPos + 1),
+                (byte)(EleElapsed + other.EleElapsed), 
+                OpenValves, 
+                (byte)(Elapsed + other.Elapsed), 
+                TotalFlow + other.TotalFlow);
         }
 
         public TwoPath NextHuman(Valve valve, int distance)
         {
-            var humanPath = new List<string>(HumanPath) { valve.Name };
-            var elePath = new List<string>(ElePath);
+            var humanPath = HumanPath[..];
+            humanPath[HumanPathPos + 1] = valve.Id;
+            var elePath = ElePath[..];
             var elapsed = HumanElapsed + distance + 1;
             var flow = (Part2Minutes - elapsed) * valve.Flow;
-            return new TwoPath(humanPath, elapsed, elePath, EleElapsed, OpenValves | valve.Mask, Math.Max(elapsed, EleElapsed), TotalFlow + flow);
+            return new TwoPath(
+                humanPath,
+                (byte)(HumanPathPos + 1),
+                (byte)elapsed, 
+                elePath,
+                ElePathPos,
+                EleElapsed, 
+                OpenValves | valve.Mask, 
+                (byte)Math.Max(elapsed, EleElapsed), 
+                TotalFlow + flow);
         }
 
         public TwoPath NextElephant(Valve valve, int distance)
         {
-            var humanPath = new List<string>(HumanPath);
-            var elePath = new List<string>(ElePath) { valve.Name };
+            var humanPath = HumanPath[..];
+            var elePath = ElePath[..];
+            elePath[ElePathPos + 1] = valve.Id;
             var elapsed = EleElapsed + distance + 1;
             var flow = (Part2Minutes - elapsed) * valve.Flow;
-            return new TwoPath(humanPath, HumanElapsed, elePath, elapsed, OpenValves | valve.Mask, Math.Max(elapsed, HumanElapsed), TotalFlow + flow);
+            return new TwoPath(
+                humanPath,
+                HumanPathPos,
+                HumanElapsed, 
+                elePath,
+                (byte)(ElePathPos + 1),
+                (byte)elapsed,
+                OpenValves | valve.Mask, 
+                (byte)Math.Max(elapsed, HumanElapsed), 
+                TotalFlow + flow);
         }
 
         public TwoPath Diff(TwoPath start)
         {
-            var humanPath = new List<string>(HumanPath.GetRange(start.HumanPath.Count, HumanPath.Count - start.HumanPath.Count));
-            var elePath = new List<string>(ElePath.GetRange(start.ElePath.Count, ElePath.Count - start.ElePath.Count));
-            return new TwoPath(humanPath, HumanElapsed - start.HumanElapsed, elePath, EleElapsed - start.EleElapsed, OpenValves, Elapsed - start.Elapsed, TotalFlow - start.TotalFlow);
+            var humanPath = new byte[HumanPath.Length];
+            if (HumanPathPos > start.HumanPathPos)
+            {
+                HumanPath[(HumanPathPos - start.HumanPathPos)..HumanPathPos].CopyTo(humanPath, 0);
+            }
+            var elePath = new byte[ElePath.Length];
+            if (ElePathPos > start.ElePathPos)
+            {
+                ElePath[(ElePathPos - start.ElePathPos)..ElePathPos].CopyTo(elePath, 0);
+            }
+            return new TwoPath(
+                humanPath,
+                (byte) (HumanPathPos - start.HumanPathPos),
+                (byte) (HumanElapsed - start.HumanElapsed),
+                elePath,
+                (byte) (ElePathPos - start.ElePathPos),
+                (byte) (EleElapsed - start.EleElapsed),
+                OpenValves,
+                (byte) (Elapsed - start.Elapsed),
+                TotalFlow - start.TotalFlow
+            );
         }
     }
 }
